d2f7c8dfe8cbc67549c2d1aca03b4d4d18d2e363
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2025-01-30 01:11:10 +0600

    updated

--- src/test/java/org/mdzahidhasanjamil/citest1/controller/UserControllerTest.java
+++ src/test/java/org/mdzahidhasanjamil/citest1/controller/UserControllerTest.java
@@ -1,32 +1,6 @@
 package org.mdzahidhasanjamil.citest1.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
-class UserControllerTest {
-
-  @Autowired private MockMvc mockMvc;
-
-  @Test
-  void contextLoads() {
-    Assertions.assertTrue(true);
-  }
-
-  @Test
-  void test() throws Exception {
-    mockMvc
-        .perform(get("/user/test"))
-        .andExpect(status().isOk()) // Check if the status is 200 OK
-        .andExpect(
-            content()
-                .string("Test successfully done with updated version")); // Verify response body
-  }
-}
+class UserControllerTest {}
